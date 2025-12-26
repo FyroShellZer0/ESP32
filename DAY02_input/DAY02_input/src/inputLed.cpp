847d00e9731ace44546ca4b698a6fbd7e2a3ae6d
raijen <raijenjb@example.com>
2025-12-26 15:35:13 +0800

    Input controls led

---
+++ DAY02_input/DAY02_input/src/inputLed.cpp
@@ -0,0 +1,47 @@
+#include <Arduino.h>
+#include <stdbool.h>
+bool On = false;
+void setup(){
+  Serial.begin(115200);
+  pinMode(2, OUTPUT);
+  // Wait for serial connection OR timeout after 10 seconds
+  unsigned long startTime = millis();
+  while(!Serial && (millis() - startTime < 10000)){
+    delay(10);
+  }
+  Serial.println("Esp32 ready!, type something...");
+
+}
+void loop() {
+  
+  if(Serial.available()){
+    //Serial.print("Enter: ");
+    String input = Serial.readStringUntil('\n');
+    input.trim();
+    
+    if(input.length() > 0){
+     Serial.print("you typed: ");
+     Serial.println(input);
+     if(input == "1"){
+       On = true;
+     }
+     else{
+       On = false;
+     }
+    
+    }
+  }
+   
+    if (On == true){
+      Serial.println("Turning on...");
+      delay(1000);
+      digitalWrite(2, HIGH);
+    }
+    if (On == false){
+      Serial.println("Turning off");
+      delay(1000);
+      digitalWrite(2, LOW);
+    }
+  
+  
+}
